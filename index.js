a7e99856a9491aa796dccb54e994cf416e73646b
dakotafabro <dakotafabrocgu@gmail.com>
2022-02-16 09:10:14 -0800

    Created and initialized web scrape

---
+++ index.js
@@ -0,0 +1,32 @@
+const PORT = 8000;
+const axios = require("axios");
+const cheerio = require("cheerio");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+
+const url = "https://www.reddit.com/";
+
+app.get("/results", () => {});
+
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const articles = [];
+
+    $(".y8HYJ-y_lTUHkQIc1mdCq", html).each(function () {
+      const title = $(this).text();
+      const url = $(this).find("a").attr("href");
+      articles.push({
+        title,
+        url,
+      });
+    });
+
+    console.log(articles);
+  })
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
